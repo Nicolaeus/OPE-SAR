f6cc82eccd9deaf37bfdbb82588665f4b4d52b70
Nicolaeus <76745056+Nicolaeus@users.noreply.github.com>
2026-03-15 19:20:36 +0100

    Create globals.js

---
+++ js/globals.js
@@ -0,0 +1,5 @@
+let vsdMarker, driftEndMarker, driftLine, navLine, rangeCircle, searchArea;
+let currentSea = 'calm', currentVisibility = 'day';
+let currentSearchPattern = null;
+let searchPatternLayers = [];
+let searchCircleLayer = null;
