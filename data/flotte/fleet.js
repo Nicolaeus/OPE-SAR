4d875fcb51903eae74d868c978df21dd4680a5cd
Nicolaeus <76745056+Nicolaeus@users.noreply.github.com>
2026-03-15 15:49:13 +0100

    Create fleet.js

---
+++ data/flotte/fleet.js
@@ -0,0 +1,8 @@
+export const fleet = {
+    nsh1: { name: "NSH1", tank: 3400, vMax: 25, rangeNominal: 50, towPenalty: 1.4, cons: "180 L/h" },
+    nsh2: { name: "NSH2", tank: 2600, vMax: 25, rangeNominal: 40, towPenalty: 1.4, cons: "140 L/h" },
+    nsc1: { name: "NSC1", tank: 1500, vMax: 30, rangeNominal: 30, towPenalty: 1.5, cons: "110 L/h" },
+    nsc2: { name: "NSC2", tank: 400,  vMax: 30, rangeNominal: 20, towPenalty: 1.6, cons: "60 L/h" },
+    nsc3: { name: "NSC3", tank: 150,  vMax: 30, rangeNominal: 6,  towPenalty: 1.7, cons: "25 L/h" },
+    nsc4: { name: "NSC4 (Jet)", tank: 45, vMax: 60, rangeNominal: 6, towPenalty: 2.0, cons: "15 L/h" }
+};
