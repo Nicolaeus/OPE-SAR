2dcdff6018214a512c5e46690d29cfd4e051834e
Nicolas <76745056+Nicolaeus@users.noreply.github.com>
2026-07-09 08:57:13 +0200

    Update splashScreen.js

--- src/shared/ui/splash/splashScreen.js
+++ src/shared/ui/splash/splashScreen.js
@@ -1,8 +1,12 @@
 export default class SplashScreen {
-
+    
     static #element = null;
 
+    static #startTime = 0;
+
     static show() {
+
+        this.#startTime = performance.now();
 
         if (this.#element) {
             return;
@@ -65,24 +69,42 @@ export default class SplashScreen {
 
     }
 
-    static hide() {
-
-        if (!this.#element) {
-            return;
-        }
-
-        this.#element.classList.add(
-            'hide'
-        );
-
-        setTimeout(() => {
-
+     static async hide() {
+    
+            const elapsed =
+                performance.now() -
+                this.#startTime;
+    
+            const remaining =
+                Math.max(
+                    1500 - elapsed,
+                    0
+                );
+    
+            await new Promise(
+                resolve =>
+                    setTimeout(
+                        resolve,
+                        remaining
+                    )
+            );
+    
+            this.#element.classList.add(
+                'hide'
+            );
+    
+            await new Promise(
+                resolve =>
+                    setTimeout(
+                        resolve,
+                        600
+                    )
+            );
+    
             this.#element.remove();
-
+    
             this.#element = null;
-
-        }, 600);
-
-    }
+    
+        }
 
 }
