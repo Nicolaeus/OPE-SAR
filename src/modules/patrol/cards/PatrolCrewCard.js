a75b29ba6a1c9d16aad087a9a06e5a27c9142c0d
Nicolas <76745056+Nicolaeus@users.noreply.github.com>
2026-07-27 21:01:01 +0200

    Update PatrolCrewCard.js

--- src/modules/patrol/cards/PatrolCrewCard.js
+++ src/modules/patrol/cards/PatrolCrewCard.js
@@ -32,7 +32,7 @@ export default class PatrolCrewCard
             existing.remove();
         }
 
-        const patrol = PatrolService.getCurrentPatrol();
+        const patrol = PatrolService.getCurrent();
 
         const card = new BaseCard({
 
