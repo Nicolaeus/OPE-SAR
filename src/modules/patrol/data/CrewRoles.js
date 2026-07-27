6ad814d54dce423267ed0d14526b2486ecc9d93e
Nicolas <76745056+Nicolaeus@users.noreply.github.com>
2026-07-27 18:52:11 +0200

    Create CrewRoles.js

---
+++ src/modules/patrol/data/CrewRoles.js
@@ -0,0 +1,58 @@
+export const CREW_ROLES = [
+
+    {
+        id: "captain",
+        label: "Patron"
+    },
+
+    {
+        id: "deputy",
+        label: "Sous-patron"
+    },
+
+    {
+        id: "pilot",
+        label: "Pilote embarcation"
+    },
+
+    {
+        id: "radio",
+        label: "Opérateur radio"
+    },
+
+    {
+        id: "navigation",
+        label: "Opérateur cartographie"
+    },
+
+    {
+        id: "mechanic",
+        label: "Mécanicien"
+    },
+
+    {
+        id: "doctor",
+        label: "Médecin"
+    },
+
+    {
+        id: "nurse",
+        label: "Infirmier"
+    },
+
+    {
+        id: "diver",
+        label: "Plongeur"
+    },
+
+    {
+        id: "observer",
+        label: "Observateur"
+    },
+
+    {
+        id: "deck",
+        label: "Équipier de pont"
+    }
+
+];
