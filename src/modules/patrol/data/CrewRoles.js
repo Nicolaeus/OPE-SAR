b7abd3b34ad67c5c65264de2e7639d2807474d83
Nicolas <76745056+Nicolaeus@users.noreply.github.com>
2026-07-30 09:58:48 +0200

    Add mandatory and multiple properties to crew roles

--- src/modules/patrol/data/CrewRoles.js
+++ src/modules/patrol/data/CrewRoles.js
@@ -2,57 +2,79 @@ export const CREW_ROLES = [
 
     {
         id: "captain",
-        label: "Patron"
+        label: "Patron",
+        mandatory: true,
+        multiple: false
     },
 
     {
         id: "deputy",
-        label: "Sous-patron"
+        label: "Sous-patron",
+        mandatory: false,
+        multiple: false
     },
 
     {
         id: "pilot",
-        label: "Pilote embarcation"
+        label: "Pilote embarcation",
+        mandatory: false,
+        multiple: false
     },
 
     {
         id: "radio",
-        label: "Opérateur radio"
+        label: "Opérateur radio",
+        mandatory: false,
+        multiple: false
     },
 
     {
         id: "navigation",
-        label: "Opérateur cartographie"
+        label: "Opérateur cartographie",
+        mandatory: false,
+        multiple: false
     },
 
     {
         id: "mechanic",
-        label: "Mécanicien"
+        label: "Mécanicien",
+        mandatory: false,
+        multiple: false
     },
 
     {
         id: "doctor",
-        label: "Médecin"
+        label: "Médecin",
+        mandatory: false,
+        multiple: false
     },
 
     {
         id: "nurse",
-        label: "Infirmier"
+        label: "Infirmier",
+        mandatory: false,
+        multiple: false
     },
 
     {
         id: "diver",
-        label: "Plongeur"
+        label: "Plongeur",
+        mandatory: false,
+        multiple: true
     },
 
     {
         id: "observer",
-        label: "Observateur"
+        label: "Observateur",
+        mandatory: false,
+        multiple: false
     },
 
     {
         id: "deck",
-        label: "Équipier de pont"
+        label: "Équipier de pont",
+        mandatory: true,
+        multiple: true
     }
 
 ];
