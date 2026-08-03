19b75fd736845794b1378fa43bd8e3bc0b439c38
Nicolas <76745056+Nicolaeus@users.noreply.github.com>
2026-08-03 16:32:59 +0200

    Update JsonExporter.js

--- src/core/exporters/JsonExporter.js
+++ src/core/exporters/JsonExporter.js
@@ -6,6 +6,67 @@
  *
  * Export d'un rapport au format JSON.
  *
+ * Le rapport doit être construit au préalable
+ * par ReportBuilder.
+ * ==========================================================
+ */
+
+import BaseExporter
+    from './BaseExporter.js';
+
+export default class JsonExporter
+    extends BaseExporter {
+
+    /**
+     * Exporte un rapport au format JSON.
+     *
+     * @param {Object} report
+     * @param {String} filename
+     *
+     * @returns {File}
+     */
+    static async export(
+
+        report,
+
+        filename = 'Export'
+
+    ) {
+
+        const json =
+
+            JSON.stringify(
+
+                report,
+
+                null,
+
+                4
+
+            );
+
+        return this.createFile(
+
+            filename,
+
+            'json',
+
+            'application/json',
+
+            json
+
+        );
+
+    }
+
+}/**
+ * ==========================================================
+ * OPE-SAR Core
+ * ----------------------------------------------------------
+ * JsonExporter.js
+ *
+ * Export d'un rapport au format JSON.
+ *
  * Ce service est indépendant des modules.
  * Il exporte uniquement un rapport normalisé.
  * ==========================================================
