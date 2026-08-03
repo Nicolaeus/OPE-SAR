9e748607b424a7d127a6043f47ce7f441220550a
Nicolas <76745056+Nicolaeus@users.noreply.github.com>
2026-08-03 16:01:37 +0200

    Update Events.js

---
+++ src/core/events/Events.js
@@ -0,0 +1,27 @@
+export default Object.freeze({
+
+    PATROL_UPDATED:
+        "patrol:updated",
+
+    PATROL_TRACK:
+        "patrol:track",
+
+    PATROL_OPEN:
+        "patrol:card:open",
+
+    PATROL_CREW:
+        "patrol:crew",
+
+    PATROL_JOURNAL:
+        "patrol:journal",
+
+    PATROL_COMMUNICATIONS:
+        "patrol:communications",
+
+    PATROL_SUMMARY:
+        "patrol:summary",
+
+    PATROL_CLOSING:
+        "patrol:closing"
+
+});
